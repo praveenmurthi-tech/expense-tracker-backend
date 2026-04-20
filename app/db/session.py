dd75c08aebf4f1fb4ff36f8f368eaaaf34d9e1fe
praveen-gritwizz <praveengritwiz@gmail.com>
2026-04-20 21:50:44 +0530

    changes in the db session

--- app/db/session.py
+++ app/db/session.py
@@ -1,18 +1,13 @@
 import os
 from sqlmodel import create_engine, Session
 
-# Get base directory of the project
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# Create DB path
-DB_PATH = os.path.join(BASE_DIR, "expenses.db")
+DB_PATH = os.path.join(os.getcwd(), "expenses.db")
 
 DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
     DATABASE_URL,
-    echo=True,
-    connect_args={"check_same_thread": False}  # needed for SQLite
+    connect_args={"check_same_thread": False}
 )
 
 
